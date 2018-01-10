a5078fe453d1c6f67c3b1950847c0011a1b68a33
Jerko Lenstra <git@lenj.org>
2018-01-10 21:15:14 +0100

    code: smart_marker: clean up main.c

--- code/smart_marker/src/main.c
+++ code/smart_marker/src/main.c
@@ -1,51 +1,29 @@
 /* Includes ------------------------------------------------------------------*/
 #include <board.h>
-#include <gps.h>
 #include <lora.h>
-
-#include <uart.h>
-#include <string.h>
-#include <stdio.h>
-#include <definitions.h>
-#include <ublox.h>
 
 /* Private typedef -----------------------------------------------------------*/
 /* Private define ------------------------------------------------------------*/
-#define SLAVE_ADDRESS (0x42 << 1)
-#define MARKER_ID 0
-
 /* Private macro -------------------------------------------------------------*/
 /* Private variables ---------------------------------------------------------*/
-uint8_t output[80];
-
 /* Private functions ---------------------------------------------------------*/
 int
 main(void)
 {
-    // struct gps_data gps_data;
+    uint8_t test_buf[] = { 0xBE, 0xEF, 0xCA, 0xCE, 0x00, 0x11, 0x22, 0x33 };
 
     board_init();
-    // gps_init(&gps_data);
     lora_init();
 
-    set_message(SLAVE_ADDRESS, GGA, 0);
-    set_message(SLAVE_ADDRESS, GNS, 2);
-    set_message(SLAVE_ADDRESS, RMC, 0);
-    set_message(SLAVE_ADDRESS, GLL, 0);
-    set_message(SLAVE_ADDRESS, VTG, 0);
-
     while (1) {
-        i2c_receive_nmea(SLAVE_ADDRESS);
-        parse_data(MARKER_ID, output);
-
         if (lora_network_is_joined()) {
-            // lora_prepare_frame(gps_data.buf, gps_data.size);
+            lora_prepare_frame(test_buf, sizeof(test_buf));
             lora_send_frame();
         } else {
             lora_join();
         }
 
-        board_sleep();
+        // board_sleep();
     }
 }
 
